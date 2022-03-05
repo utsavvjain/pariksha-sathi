fa53b90cff70e750641b229ed652da275c1e7571
Utsav <utsavj502@gmail.com>
2022-03-05 14:41:08 +0530

    initialized backend

---
+++ src/main/java/com/pariksha/sathi/pariksha_sathi/ParikshaSathiApplication.java
@@ -0,0 +1,13 @@
+package com.pariksha.sathi.pariksha_sathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParikshaSathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParikshaSathiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pariksha/sathi/pariksha_sathi/ParikshaSathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pariksha.sathi.pariksha_sathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParikshaSathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
